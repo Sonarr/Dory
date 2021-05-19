9c7612bc4ef7df769c2238240132cb50f737cfa4
Taloth Saldono <Taloth@users.noreply.github.com>
2021-05-19 23:36:03 +0200

    Secrets is optional

--- hubot-start.sh
+++ hubot-start.sh
@@ -5,7 +5,9 @@ export user=$(whoami)
 export PORT="9889"
 export HUBOT_AUTH_ADMIN="markus101"
 
-. ./secrets.sh
+if [ -f "./secrets.sh" ]; then
+  . ./secrets.sh
+fi
 
 #cd /home/hubot/hubot
 #cp hubot-scripts-deploy.json hubot-scripts.json
